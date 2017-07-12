ac4aa63d9baa26112aff55a06065030827c329af
Blaza <radebeka@gmail.com>
2017-07-12 18:04:11 +0200

    Add shape classification

---
+++ R/shape_classifier.R
@@ -0,0 +1,31 @@
+#' @export
+shape_code_map <- list("et" = "equilateral triangle",
+                       "rt" = "right triangle",
+                       "tr" = "triangle",
+                       "sq" = "square",
+                       "re" = "rectangle",
+                       "rh" = "rhombus",
+                       "pa" = "paralelogram",
+                       "tp" = "trapezium",
+                       "ci" = "circle",
+                       "el" = "ellipse",
+                       "pe" = "pentagon",
+                       "he" = "hexagon")
+
+#' Classify a shape
+#'
+#' Classifies the shape into one of the categories which can be seen from
+#' shape_code_map
+#'
+#' @param shape - the shape to classify
+#' @return A character vector with the code and long name of the shape class
+#' @export
+classify_shape <- function(shape) {
+  # In the model we used -1 for non existant values, e.g. angle of circle
+  shape_pred <- get_shape_predictors(shape, df = TRUE, na_replacement = -1)
+
+  shape_class <- as.character(predict(shapes_model, shape_pred))
+
+  c(code = shape_class, long_name = shape_code_map[[shape_class]])
+}
+
